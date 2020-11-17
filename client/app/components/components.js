1aaaa16b7959e1dde3f415f2f30e228143d632e9
Fyodor Ananiev <tedmx@ya.ru>
2020-11-17 11:53:35 +0000

    Setup simple binding

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,9 +1,7 @@
 import angular from 'angular';
-import PostModule from './post/post.module';
 import CommentModule from './comment/comment.module';
 
 const ComponentsModule = angular.module('app.components', [
-  PostModule.name, 
   CommentModule.name 
 ]);
 
--- client/app/components/post/post.module.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-import postComponent from './post.component';
-
-const postModule = angular.module('post', [])
-  .component('post', postComponent);
-export default postModule;
